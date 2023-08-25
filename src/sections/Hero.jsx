c971e95652aa0ba3e0312304e89e30925bbc8708
Ayush Kumar <ayushkumar1708@gmail.com>
2023-08-25 23:37:35 +0530

    complete the design of hero section and popular products section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,18 +3,24 @@ import { arrowRight } from "../assets/icons";
 import { statistics, shoes } from "../constants";
 import { bigShoe1 } from "../assets/images";
 import ShoeCard from "../components/ShoeCard";
+import { useState } from "react";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
   return (
     <section
       id="main"
-      className="w-full flex flex-col xl:flex-row justify-center min-h-screen
-    gap-10 max-container border-2 "
+      className="w-full  flex flex-col xl:flex-row justify-center min-h-screen
+    gap-10 max-container "
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 "  >The New Arrival</span>
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 ">
+            The New Arrival
+          </span>
           <br />
           <span className="text-coral-red mt-3 inline-block">Nike</span> Shoes
         </h1>
@@ -27,22 +33,36 @@ const Hero = () => {
           {statistics.map((stat) => {
             return (
               <div key={stat.label}>
-                <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-                <p className="font-montserrat leading-7 text-slate-gray">{stat.label}</p>
+                <p className="text-4xl font-palanquin font-bold">
+                  {stat.value}
+                </p>
+                <p className="font-montserrat leading-7 text-slate-gray">
+                  {stat.label}
+                </p>
               </div>
             );
           })}
         </div>
       </div>
       <div className="relative flex-1 flex  justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
-        <img src={bigShoe1} alt="shoe-collection" width={610} height={500} className='object-contain relative z-10' />
-      </div>
-      <div>
-        {shoes.map((shoe,index)=>(
-          <div key={shoe}>
-            <ShoeCard imgURL={shoe} changeBigShoeImg={()=>{}} bigShoe="" />
-          </div>
-        ))}
+        <img
+          src={bigShoeImg}
+          alt="shoe-collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImg={(shoe) => {setBigShoeImg(shoe)}}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
